70864b9d45a065bdcfd749382b81dd582e291528
KubaC <coufalovi.btc@gmail.com>
2023-11-26 21:02:36 +0100

    Splneny ukol cislo 3

--- shoppinglist/src/App.js
+++ shoppinglist/src/App.js
@@ -1,153 +1,187 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus, faList, faTrash } from '@fortawesome/free-solid-svg-icons';
-import Switch from '@mui/material/Switch';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import ShoppingList from './ShoppingList';
+import { faPlus, faTrash, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import ShoppingList from './ShoppingList';
 
+const App = () => {
+  const [data, setData] = useState({
+    lists: [
+      { listName: 'MyList1', listOwner: 'You', items: [] }, //Jako v ukolu na BE
+    ],
+  });
 
-const App = ({ }) => {
-
-  const [isToggled, setIsToggled] = useState(true);
-
-  const [items, setItems] = useState([
-    { listName: 'MyList1', listOwner: 'You' },
-  ]);
-
-  const displayCompleted = true;
-  //input const na přidávání položek
+  const [totalItemCount, setTotalItemCount] = useState(0);
   const [inputValue, setInputValue] = useState('');
   const [inputValueList, setInputValueList] = useState('');
 
-  const [totalItemCount, setTotalItemCount] = useState(3);
-
-  const handleChangeViewCompleted = () => {
-    setIsToggled(!isToggled);
-  };
-
-
-
-  //Handle AddItem
-  const handleAddItem = () => {
+  const handleAddItem = (listIndex, inputValue) => {
     const newItem = {
-      listName: inputValue,
-      listOwner: "You"
-    }
-    if (!newItem.listName) {
-      newItem.listName = "My new list"
-    }
-    const newItems = [...items, newItem];
+      itemName: inputValue,
+      quantity: 1,
+      isSelected: false,
+    };
 
-    setItems(newItems);
+    setData((prevData) => {
+      const newLists = [...prevData.lists];
+      newLists[listIndex].items = [...newLists[listIndex].items, newItem];
 
-    setInputValue('');
+      return { lists: newLists };
+    });
 
-    // Manually increment totalItemCount by 1
-    setTotalItemCount(prevTotalItemCount => prevTotalItemCount + 1);
-
-    //Pro animaci
-
-  };
-
-
-  //Handle pridat pocet - Prijme parametr Index aby jsme vedeli jaky item ma pridat quantity
-  const handleQuantityPlus = (index) => {
-    const newItems = [...items];
-
-    newItems[index].quantity++;
-
-    setItems(newItems);
     calculateTotal();
-  }
+  };
+//Prida pocet v konkretnim listu a u konkretniho item indexu
+  const handleQuantityPlus = (listIndex, itemIndex) => {
+    setData((prevData) => {
+      const newLists = [...prevData.lists];
+      const newItems = [...newLists[listIndex].items];
+      newItems[itemIndex].quantity++;
+      newLists[listIndex].items = newItems;
 
-  //handle odebrat pocet
-  const handleQuantityMinus = (index) => {
-    const newItems = [...items];
-    newItems.splice(index, 1);
-    setItems(newItems);
+      return { lists: newLists };
+    });
 
+    calculateTotal(); //Update total count
   };
 
-  //Spocitat total
+  const handleQuantityMinus = (listIndex, itemIndex) => {
+    setData((prevData) => {
+      const newLists = [...prevData.lists];
+      const newItems = [...newLists[listIndex].items];
+
+      if (newItems[itemIndex].quantity > 1) {
+        newItems[itemIndex].quantity--;
+      } else {
+        newItems.splice(itemIndex, 1);
+      }
+
+      newLists[listIndex].items = newItems;
+
+      return { lists: newLists };
+    });
+
+    calculateTotal();
+  };
+
+  const removeList = (listIndex) => {
+    setData((prevData) => {
+      const newLists = [...prevData.lists];
+      newLists.splice(listIndex, 1);
+      return { lists: newLists };
+    });
+  };
+
   const calculateTotal = () => {
-    const totalItemCount = items.reduce((total, item) => {
-      return total + item.quantity;
+    const totalItemCount = data.lists.reduce((total, list) => {
+      return total + list.items.reduce((listTotal, item) => (item.isSelected ? listTotal + item.quantity : listTotal), 0);
     }, 0);
     setTotalItemCount(totalItemCount);
   };
-
-  const handleEditListName = (index) => {
-    console.log("EDIT");
-    const newItems = [...items];
-    newItems[index].listName = inputValueList;
-    setItems(newItems);
+  //Pridat novy list
+  const addNewShoppingList = () => {
+    const newList = { listName: inputValue, listOwner: 'You', items: [] };
+    setData((prevData) => ({ lists: [...prevData.lists, newList] }));
+    setInputValue('');
+  };
+//pro editaci listu
+  const handleEditListName = (listIndex) => {
+    setData((prevData) => {
+      const newLists = [...prevData.lists];
+      newLists[listIndex].listName = inputValueList;
+      return { lists: newLists };
+    });
+  };
+  const handleToggleComplete = (listIndex, itemIndex) => {
+    setData((prevData) => {
+      const newLists = [...prevData.lists];
+      newLists[listIndex].items[itemIndex].isSelected = !newLists[listIndex].items[itemIndex].isSelected;
+      return { lists: newLists };
+    });
   };
 
+  useEffect(() => {
+    calculateTotal();
+  }, [data.lists]);
 
   return (
-    <div className='app-background'>
-      <div className='main-container'>
-        <div className='add-item-box'>
-          <input
-            value={inputValue}
-            onChange={(event) => setInputValue(event.target.value)}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                handleAddItem();
+    <Router>
+      <div className='app-background'>
+        <div className='main-container'>
+          <div className='add-item-box'>
+            <input
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  addNewShoppingList();
+                }
+              }}
+              className='add-item-input'
+              placeholder='Přidat list...'
+            />
+            <FontAwesomeIcon icon={faPlus} onClick={addNewShoppingList} />
+          </div>
+          <div className='item-list'>
+            <TransitionGroup>
+              {data.lists.map((list, listIndex) => (
+                <CSSTransition key={listIndex} timeout={500} classNames="item">
+                  <div className='item-container'>
+                    <div className='item-name'>
+                      <span>
+                        <input
+                          onKeyDown={(event) => {
+                            if (event.key === 'Enter') {
+                              handleEditListName(listIndex);
+                            }
+                          }}
+                          onChange={(event) => setInputValueList(event.target.value)}
+                          className="change-list-name"
+                          placeholder={list.listName}
+                        />
+                      </span>
+                    </div>
+                    <Link to={`/shopping-list/${listIndex}`}>
+                      <FontAwesomeIcon icon={faInfoCircle}/>
+                    </Link>
+                    <div>
+                      <button>
+                        <FontAwesomeIcon
+                          icon={faTrash}
+                          className="beat-animation"
+                          onClick={() => removeList(listIndex)}
+                        />
+                      </button>
+                    </div>
+                  </div>
+                </CSSTransition>
+              ))}
+            </TransitionGroup>
+          </div>
+        </div>
+        <Routes>
+          {data.lists.map((list, listIndex) => (
+            <Route
+              key={listIndex}
+              path={`/shopping-list/${listIndex}`}
+              element={
+                <ShoppingList
+                  listName={list.listName}
+                  listOwner={list.listOwner}
+                  items={list.items}
+                  onAddItem={(inputValue) => handleAddItem(listIndex, inputValue)}
+                  onQuantityPlus={(itemIndex) => handleQuantityPlus(listIndex, itemIndex)}
+                  onQuantityMinus={(itemIndex) => handleQuantityMinus(listIndex, itemIndex)}
+                  onToggleComplete={(itemIndex) => handleToggleComplete(listIndex, itemIndex)}
+                />
               }
-            }}
-            className='add-item-input'
-            placeholder='Přidat list...'
-          />
-          <FontAwesomeIcon icon={faPlus} onClick={() => handleAddItem()} />
-        </div>
-        <div className='item-list'>
-          <TransitionGroup>
-            {items.map((item, index) => (
-              <CSSTransition key={index} timeout={500} classNames="item">
-                <div className='item-container'>
-                  <div className='item-name'>
-                    <span>
-                      <input
-                        onKeyDown={(event) => {
-                          if (event.key === 'Enter') {
-                            handleEditListName(index);
-                          }
-                        }}
-                        onChange={(event) => setInputValueList(event.target.value)}
-                        className="change-list-name"
-                        placeholder={item.listName}
-                      />
-                    </span>
-                  </div>
-                  <div>
-                    <button>
-                      <FontAwesomeIcon
-                        icon={faTrash}
-                        className="beat-animation"
-                        onClick={() => handleQuantityMinus(index)}
-                      />
-                    </button>
-                  </div>
-                </div>
-              </CSSTransition>
-            ))}
-          </TransitionGroup>
-        </div>
-      </div>
-      <div id='01' className="shopping-list-container">
-        <TransitionGroup className="shopping-list-container">
-          {items.map((item, index) => (
-            <CSSTransition key={index} timeout={500} classNames="item">
-              <ShoppingList key={index} listName={item.listName} listOwner={item.listOwner} />
-            </CSSTransition>
+            />
           ))}
-        </TransitionGroup>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 };
 
--- shoppinglist/src/ShoppingList.js
+++ shoppinglist/src/ShoppingList.js
@@ -1,180 +1,107 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus, faList, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
 import Switch from '@mui/material/Switch';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import { alignProperty } from '@mui/material/styles/cssUtils';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
+const ShoppingList = ({ listName, listOwner, items, onAddItem, onQuantityPlus, onQuantityMinus, onToggleComplete}) => {
+  const [isToggled, setIsToggled] = useState(true);
+  const [localInputValue, setLocalInputValue] = useState(''); // Rename to localInputValue
+  const [inputValueList, setInputValueList] = useState('');
+  const totalItemCount = items.reduce((total, item) => (!item.isSelected ? total + item.quantity : total), 0);
+  const handleChangeViewCompleted = () => {
+    setIsToggled(!isToggled);
+  };
 
-
-const ShoppingList = ({ listName, listOwner }) => {
-
-    const [isToggled, setIsToggled] = useState(true);
-    const [items, setItems] = useState([]);
-
-    const displayCompleted = true;
-    //input const na přidávání položek
-    const [inputValue, setInputValue] = useState('');
-    const [inputValueList, setInputValueList] = useState('');
-
-    const [totalItemCount, setTotalItemCount] = useState(0);
-
-    const handleChangeViewCompleted = () => {
-        setIsToggled(!isToggled);
-    };
-
-
-    //Handle AddItem
-    const handleAddItem = () => {
-        const newItem = {
-            itemName: inputValue,
-            quantity: 1,
-            isSelected: false,
-        }
-
-        const newItems = [...items, newItem];
-
-        setItems(prevItems => [...prevItems, newItem]);
-
-        setInputValue('');
-
-        // Manually increment totalItemCount by 1
-        setTotalItemCount(prevTotalItemCount => prevTotalItemCount + 1);
-
-    };
-
-    //Handle pridat pocet - Prijme parametr Index aby jsme vedeli jaky item ma pridat quantity
-    const handleQuantityPlus = (index) => {
-        const newItems = [...items];
-
-        newItems[index].quantity++;
-
-        setItems(newItems);
-        calculateTotal();
+  const handleAddItem = () => {
+    if (localInputValue.trim() !== '') {
+      onAddItem(localInputValue); // Pass localInputValue to onAddItem
+      setLocalInputValue(''); // Reset localInputValue after adding item
     }
+  };
 
-    //handle odebrat pocet
-    const handleQuantityMinus = (index) => {
-        const newItems = [...items];
-        if (newItems[index].quantity == 1) {
-            newItems.splice(index, 1);
-            setItems(newItems);
-            // Manually decrease totalItemCount by 1
-            setTotalItemCount(prevTotalItemCount => prevTotalItemCount - 1);
-            calculateTotal();
-        }
-        else {
-            newItems[index].quantity--;
-            setItems(newItems);
-            calculateTotal();
-        };
-    };
+  const handleQuantityPlus = (itemIndex) => {
+    onQuantityPlus(itemIndex);
+  };
 
-    const toggleComplete = (index) => {
-        const newItems = [...items];
-        newItems[index].isSelected = !newItems[index].isSelected;
-        setItems(newItems);
-        calculateTotal();
-    };
+  const handleQuantityMinus = (itemIndex) => {
+    onQuantityMinus(itemIndex);
+  };
 
-    //Spocitat total
-    const calculateTotal = () => {
-        const totalItemCount = items.reduce((total, item) => {
-            if (!item.isSelected) {
-                return total + item.quantity;
-            }
-            return total;
-        }, 0);
-        setTotalItemCount(totalItemCount);
-        console.log(totalItemCount);
-    };
+  const toggleComplete = (itemIndex) => {
+    onToggleComplete(itemIndex);
+  };
 
-    const getFilteredItems = () => {
-        if (!isToggled) {
-            return items.filter((item) => !item.isSelected); // Show non-completed items
-        } else {
-            return items; // Show all items
-        }
-    };
+  const getFilteredItems = () => {
+    if (!isToggled) {
+      return items.filter((item) => !item.isSelected); // Show non-completed items
+    } else {
+      return items; // Show all items
+    }
+  };
 
-    return (
-        
-        <div className='main-container'>
-            <div>
-                <div className='owner-and-name'>
-                    <span>
-                        {listName}
-                    </span>
-                    <span className='owner'>
-                        <FontAwesomeIcon icon={faUser} />
-                        {listOwner}
-
-                    </span>
-                </div>
-            </div>
-            <div className='add-item-box'>
-                <input
-                    value={inputValue}
-                    onChange={(event) => setInputValue(event.target.value)}
-                    onKeyDown={(event) => {
-                        if (event.key === 'Enter') {
-                            handleAddItem();
-                        }
-                    }}
-                    className='add-item-input'
-                    placeholder='Přidat položku...'
-                />
-
-                <FontAwesomeIcon icon={faPlus} onClick={() => handleAddItem()} /> {/* Ikona symbolu plus */}
-            </div>
-            <div className='item-list'>
-                {getFilteredItems().map((item, index) => <div className='item-container'>
-                    <div className='item-name' onClick={() => toggleComplete(index)}>
-                        {item.isSelected ? (
-                            <>
-                                <FontAwesomeIcon icon={faCheckCircle} /> {/* Ikona checkboxu */}
-                                <span className='completed'>{item.itemName}</span>
-                            </>
-                        ) : (
-                            <>
-                                <FontAwesomeIcon icon={faCircle} />
-                                <span>{item.itemName}</span>
-                            </>
-                        )}
-                    </div>
-                    <div className='quantity'>
-                        <button>
-                            <FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityMinus(index)} />
-                        </button>
-                        <span> {item.quantity} </span>
-                        <button>
-                            <FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityPlus(index)} />
-                        </button>
-                    </div>
-                </div>)}
-                <FormGroup>
-                    <FormControlLabel control={<Switch defaultChecked={isToggled} onChange={() => handleChangeViewCompleted()} />} label="Zobrazit splněné" />
-                </FormGroup>
-                <div className='total'>
-                    Total: {totalItemCount}</div>
-            </div>
+  return (
+    <div className='main-container'>
+      <div>
+        <div className='owner-and-name'>
+          <span>{listName}</span>
+          <span className='owner'>
+            <FontAwesomeIcon icon={faUser} />
+            {listOwner}
+          </span>
         </div>
-
-    );
+      </div>
+      <div className='add-item-box'>
+        <input
+          value={localInputValue}
+          onChange={(event) => setLocalInputValue(event.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              handleAddItem();
+            }
+          }}
+          className='add-item-input'
+          placeholder='Přidat položku...'
+        />
+        <FontAwesomeIcon icon={faPlus} onClick={handleAddItem} />
+      </div>
+      <div className='item-list'>
+        {getFilteredItems().map((item, index) => (
+          <div className='item-container' key={index}>
+            <div className='item-name' onClick={() => toggleComplete(index)}>
+              {item.isSelected ? (
+                <>
+                  <FontAwesomeIcon icon={faCheckCircle} />
+                  <span className='completed'>{item.itemName}</span>
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon icon={faCircle} />
+                  <span>{item.itemName}</span>
+                </>
+              )}
+            </div>
+            <div className='quantity'>
+              <button>
+                <FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityMinus(index)} />
+              </button>
+              <span>{item.quantity}</span>
+              <button>
+                <FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityPlus(index)} />
+              </button>
+            </div>
+          </div>
+        ))}
+        <FormGroup>
+          <FormControlLabel control={<Switch defaultChecked={isToggled} onChange={handleChangeViewCompleted} />} label='Zobrazit splněné' />
+        </FormGroup>
+        <div className='total'>Total: {totalItemCount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingList;
